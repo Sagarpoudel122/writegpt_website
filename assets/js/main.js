e171ebd31ca9b9b43f9542c0b68813fad4fa348a
Profiroiu Mihai <sobhakharpoudel97@gmail.com>
2023-03-01 15:37:01 +0545

    new Design Responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,51 @@
-function  openChromeExtension(){
-  window.open("https://chrome.google.com/webstore/detail/writegpt-openais-gpt-prom/dflcdbibjghipieemcligeelbmackgco", "_blank");
-  }
+function openChromeExtension() {
+  window.open(
+    "https://chrome.google.com/webstore/detail/writegpt-openais-gpt-prom/dflcdbibjghipieemcligeelbmackgco",
+    "_blank"
+  );
+}
 
+function openTryOut() {
+  window.open("https://app.writegpt.ai/", "_blank");
+}
 
-  function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-  }
+function openWatchOnYoutube() {
+  window.open("https://www.youtube.com/@writegpt-becomesuperhumanwithc", "_blank");
+}
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
 
-  /* Close when someone clicks on the "x" symbol inside the overlay */
-  function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-  }
-  document.addEventListener("DOMContentLoaded", function (event) {
-    var scrollpos = localStorage.getItem('scrollpos');
-    if (scrollpos) window.scrollTo(0, scrollpos);
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  var scrollpos = localStorage.getItem("scrollpos");
+  if (scrollpos) window.scrollTo(0, scrollpos);
+});
+
+window.onbeforeunload = function (e) {
+  localStorage.setItem("scrollpos", window.scrollY);
+};
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    var icon = this.children[0];
+    icon.className = "";
+
+    if (content.style.display === "block") {
+      content.style.display = "none";
+      icon.className = "fas fa-chevron-down";
+    } else {
+      content.style.display = "block";
+      icon.className = "fas fa-chevron-up";
+    }
   });
-
-  window.onbeforeunload = function (e) {
-    localStorage.setItem('scrollpos', window.scrollY);
-  };
+}
